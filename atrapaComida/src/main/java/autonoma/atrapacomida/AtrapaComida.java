cc81f966f6dcf841ef82e4a11040c3839aae79ac
Juan Diego Pinilla <juand.pinillaa@autonoma.edu.co>
2025-05-21 16:56:05 -0500

    proyecto netBeans creado

---
+++ atrapaComida/src/main/java/autonoma/atrapacomida/AtrapaComida.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package autonoma.atrapacomida;
+
+/**
+ *
+ * @author juand
+ */
+public class AtrapaComida {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
